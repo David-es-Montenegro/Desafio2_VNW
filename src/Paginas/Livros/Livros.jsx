6aee8315b8fc05de23f4cdf9e23ca45465b3ce6a
David Montenegro <davidmontenegronet@gmail.com>
2024-12-05 04:18:08 -0300

    Adicionada pagina "Livros doados"

--- src/Paginas/Livros/Livros.jsx
+++ src/Paginas/Livros/Livros.jsx
@@ -2,6 +2,13 @@ import styles from './Livros.module.scss';
 
 const Livros = () => (
   <div className={styles.livros}>
+    <p className={styles.livrosTxt}>Livros Doados</p>
+    <section className={styles.containerCards}>
+      <section className={styles.cardLivros}>
+        <img className={styles.cardImg} src="src\assets\download 1.png" alt="Imagem de um livro" />
+        <h2 className={styles.cardDescricao}> O protagonista Susanne Andrade Ficção</h2>
+      </section>
+    </section>
   </div>
 );
 
